6f71e5a65afbf5bb07c83bafc1eb44c105fcd2f6
Reuben Sweetman <65689588+bstabs-alt@users.noreply.github.com>
2023-01-30 11:54:21 +1300

    basic web scrape

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TradeMe-Webscraper
+
+#use pip3 freeze > requirements.txt for upto date packages
--- tm-webscraper.py
+++ tm-webscraper.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #could use Requests-HTML instead of beautiful soup (uses less code)
 
+from bs4 import BeautifulSoup
+import requests
+
+site_url = requests.get("https://www.trademe.co.nz/a/search")
+
+def main():
+    """main function"""
+    soup = BeautifulSoup(site_url.text, "html.parser")
+    print(soup.title)
+    get_tm_data(soup)
+
+def get_tm_data(soup):
+    listings = soup.find()
+    listings_rows = [row for row in listings.contents if row != "\n"]
+    print(listings_rows)
+
+if __name__ == '__main__':
+    main()
+    
+    
